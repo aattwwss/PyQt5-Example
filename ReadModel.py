22640cd7e8fac9dbe967aac710567efd9a96bdd1
aattwwss <alvintan95@gmail.com>
2019-11-04 16:23:56 +0800

    update

--- ReadModel.py
+++ ReadModel.py
@@ -5,17 +5,12 @@ import pickle
 
 f = open("decisiontree.pkl", 'rb')
 model = pickle.load(f)
-mean = pickle.load(f)
-std = pickle.load(f)
-meanY = pickle.load(f)
-stdY = pickle.load(f)
 
 def predict(features):
     
     features = np.array([features])
-    features = (features- mean/ std)
     predictions = model.predict(features)
 
-    predictions = predictions * meanY + stdY
     return predictions
+
 
